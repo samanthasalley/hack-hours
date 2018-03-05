ae7f1754b127792f07013940765af96014a9acef
Samantha Salley <samanthasalley@gmail.com>
2018-03-05 10:14:09 -0800

    second accepted solution - preferred - O(n) time, O(1) space

--- rem-nth-node-from-end.js
+++ rem-nth-node-from-end.js
@@ -12,45 +12,83 @@
  * ==========================
  */
 
+// /**
+//  * @param {ListNode} head
+//  * @param {number} n
+//  * @return {ListNode}
+//  */
+// const removeNthFromEnd = (head, n) => {
+//   // keep track of length
+//   let len = 0;
+  
+//   // loop through whole list
+//   let cur = head;
+//   do {
+//       len++;
+//       cur = cur.next;
+//   }
+//   while (cur);
+  
+//   // determine nth to last node position
+//   const rem = len - n;
+  
+//   // handle removing head
+//   if (rem === 0) {
+//       head = head.next;
+//       return head;
+//   }
+  
+//   // loop through again to the nth position
+//   let pos = 0;
+//   let prev = head;
+//   cur = head;
+//   while (pos < rem) {
+//       prev = cur;
+//       cur = cur.next;
+//       pos++;
+//   }
+  
+//   // remove the nth node
+//   prev.next = cur.next;
+  
+//   // return head
+//   return head;
+// };
+
+/**
+ * ===========================
+ * LEETCODE PREFERRED SOLUTION
+ * ===========================
+ */
+
+
 /**
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
  */
 const removeNthFromEnd = (head, n) => {
-  // keep track of length
-  let len = 0;
+  // create dummy node
+  const dummyNode = new ListNode(0);
+  dummyNode.next = head;
   
-  // loop through whole list
-  let cur = head;
-  do {
-      len++;
-      cur = cur.next;
-  }
-  while (cur);
+  // set first and second pointer to dummy node
+  let first = second = dummyNode;
   
-  // determine nth to last node position
-  const rem = len - n;
-  
-  // handle removing head
-  if (rem === 0) {
-      head = head.next;
-      return head;
+  // create for loop to n + 1, update first to n+1 node
+  for (let i = 0; i < n + 1; i++) {
+      first = first.next;
   }
   
-  // loop through again to the nth position
-  let pos = 0;
-  let prev = head;
-  cur = head;
-  while (pos < rem) {
-      prev = cur;
-      cur = cur.next;
-      pos++;
+  // loop while first exists
+  while (first) {
+      first = first.next;
+      second = second.next;
   }
   
-  // remove the nth node
-  prev.next = cur.next;
+  // once it doesn't, set second.next to second.next.next
+  second.next = second.next.next
   
-  // return head
-  return head;
+  // then return head
+  return dummyNode.next;
 };
